091e80931aca0df6bd0391af5dc5309954bd22c9
Jany Gaal <jany.gaal@hva.nl>
2021-08-26 08:52:01 +0200

    JdbcCustomerDaoTest

--- src/test/java/com/example/digital_gold/repository/JdbcCustomerDaoTest.java
+++ src/test/java/com/example/digital_gold/repository/JdbcCustomerDaoTest.java
@@ -1,12 +1,17 @@
 package com.example.digital_gold.repository;
 
+import com.example.digital_gold.domain.Address;
+import com.example.digital_gold.domain.Customer;
+import com.example.digital_gold.domain.CustomerDetails;
+import com.example.digital_gold.domain.FullName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.*;
+import java.sql.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles ("test")
@@ -24,10 +29,28 @@ public class JdbcCustomerDaoTest {
         assertThat(jdbcCustomerDaoTest).isNotNull();
     }
     @Test
-    void save() {
+    public void save() {
+        FullName testfullname = new FullName("Tester", "van", "Tester");
+        Address testadress = new Address(1, "TestStraat", "1111AA", "TestCity");
+        CustomerDetails testcustomerDetails = new CustomerDetails(Date.valueOf("1900-01-01"),"753654852",
+                "tester@gmail.com" );
+        Customer testcustomer = new Customer("TestUser10", "TestPassword", "zoutje", testfullname,
+                testadress, testcustomerDetails);
+        Customer actualcustomer = jdbcCustomerDaoTest.save(testcustomer);
+        assertThat(actualcustomer).isEqualTo(testcustomer);
     }
 
     @Test
-    void findCustomerByUsername() {
+    public void findCustomerByUsernameTrue() {
+        Boolean actual = jdbcCustomerDaoTest.findCustomerByUsername("TestUser01");
+        Boolean expected = true;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void findCustomerByUsernameFalse() {
+        Boolean actual = jdbcCustomerDaoTest.findCustomerByUsername("TestUser05");
+        Boolean expected = false;
+        assertThat(actual).isEqualTo(expected);
     }
 }
