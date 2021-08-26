c259491a98d0ada96b96280b5dc1077680e3a954
Jany Gaal <jany.gaal@hva.nl>
2021-08-26 11:00:12 +0200

    ServiceRepoIntergrationTest

--- src/test/java/com/example/digital_gold/ServiceRepoIntegrationTest.java
+++ src/test/java/com/example/digital_gold/ServiceRepoIntegrationTest.java
@@ -1,4 +1,47 @@
 package com.example.digital_gold;
-//TODO Jany
+
+import com.example.digital_gold.domain.Address;
+import com.example.digital_gold.domain.Customer;
+import com.example.digital_gold.domain.CustomerDetails;
+import com.example.digital_gold.domain.FullName;
+import com.example.digital_gold.repository.RootRepository;
+import com.example.digital_gold.service.RegisterService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Date;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
 public class ServiceRepoIntegrationTest {
+
+   private RegisterService registerService;
+
+   @Autowired
+    public ServiceRepoIntegrationTest(RegisterService registerService) {
+        this.registerService = registerService;
+    }
+
+    @Test
+    public void registerServiceNotNull() {
+       assertThat(registerService).isNotNull();
+    }
+
+    @Test
+    public void rootRepositoryNotNull() {
+       assertThat(registerService.getRootRepository()).isNotNull();
+    }
+
+    @Test
+    public void registerServiceTest() {
+        FullName testfullname = new FullName("Tester", "van", "Tester");
+        Address testadress = new Address(1, "TestStraat", "1111AA", "TestCity");
+        CustomerDetails testcustomerDetails = new CustomerDetails(Date.valueOf("1900-01-01"),"753654852",
+                "tester@gmail.com" );
+        Customer testcustomer = new Customer("TestUser10", "TestPassword", "zoutje", testfullname,
+                testadress, testcustomerDetails);
+        assertThat(registerService.register(testcustomer)).isEqualTo(testcustomer);
+    }
 }
