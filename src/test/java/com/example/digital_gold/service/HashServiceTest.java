09388948c5399b88c26d7c4e99c0b352506b6892
David Truijens <david.truijens@hva.nl>
2021-08-26 11:05:57 +0200

    hashservicetest

--- src/test/java/com/example/digital_gold/service/HashServiceTest.java
+++ src/test/java/com/example/digital_gold/service/HashServiceTest.java
@@ -3,10 +3,25 @@ package com.example.digital_gold.service;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-//TODO David
+// David
 class HashServiceTest {
 
     @Test
-    void hash() {
+    void hashReturnedIsNotNull(){
+        HashService hashService = new HashService(new PepperService());
+        String hash = hashService.hash("TestString");
+        assertNotNull(hash);
+    }
+
+    @Test
+    void hashIsDeterministic() {
+        HashService hashService1 = new HashService(new PepperService());
+        String hash1 = hashService1.hash("TestString");
+        HashService hashService2 = new HashService(new PepperService());
+        String hash2 = hashService2.hash("TestString");
+        String hash3 = hashService1.hash("Teststrin");
+        assertEquals(hash1,hash2);
+        assertNotEquals(hash1,hash3);
+        assertTrue(hash1.length() == 64 && hash2.length() == 64 && hash3.length() == 64);
     }
 }
