a926f8c72c5beba2d8bcb9b4bf6632f095dc48f3
David Truijens <david.truijens@hva.nl>
2021-09-03 14:19:23 +0200

    RootRepo aanpassing Transaction

--- src/main/java/com/example/digital_gold/repository/RootRepository.java
+++ src/main/java/com/example/digital_gold/repository/RootRepository.java
@@ -2,6 +2,7 @@ package com.example.digital_gold.repository;
 
 import com.example.digital_gold.domain.Administrator;
 import com.example.digital_gold.domain.Customer;
+import com.example.digital_gold.domain.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,13 @@ public class RootRepository {
 
     private CustomerDao customerDao;
     private AdministratorDao administratorDao;
+    private TransactionDao transactionDao;
 
     @Autowired
-    public RootRepository(CustomerDao customerDao, AdministratorDao administratorDao) {
+    public RootRepository(CustomerDao customerDao, AdministratorDao administratorDao, TransactionDao transactionDao) {
         this.customerDao = customerDao;
         this.administratorDao = administratorDao;
+        this.transactionDao = transactionDao;
         logger.info("New RootRepository");
     }
 
@@ -42,6 +45,10 @@ public class RootRepository {
 
     public String findCustomerHashPassword(String username){return customerDao.findCustomerHashPassword(username);}
     public String findAdministratorHashPassword(String username){return administratorDao.findAdministratorHashPassword(username);}
+
+    public Transaction saveTransaction(Transaction transaction) {
+        return transactionDao.saveTransaction(transaction);
+    }
 
 
 
