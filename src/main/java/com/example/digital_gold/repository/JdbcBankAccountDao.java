bcb1ab0f4e4aabb1d51d0cd3ccba347f47bade2c
sandraturina <sandraturina@sandras-mbp-2.home>
2021-09-04 14:13:08 +0200

    BankAccount Dao incl Test, toegevoegd aan RootRepository git status
    git

---
+++ src/main/java/com/example/digital_gold/repository/JdbcBankAccountDao.java
@@ -0,0 +1,54 @@
+package com.example.digital_gold.repository;
+
+import com.example.digital_gold.domain.Asset;
+import com.example.digital_gold.domain.BankAccount;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *  @author Sandra Turina
+ * */
+
+@Repository
+public class JdbcBankAccountDao implements BankAccountDao{
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcBankAccountDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private PreparedStatement insertBankAccountStatement(BankAccount bankAccount, Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "insert into BankAccount (iban, balance) values (?, ?)");
+        preparedStatement.setString(1, bankAccount.getIban());
+        preparedStatement.setDouble(2, bankAccount.getBalance());
+        return preparedStatement;
+    }
+
+    @Override
+    public BankAccount saveBankAccount(BankAccount bankAccount) {
+        jdbcTemplate.update(connection -> insertBankAccountStatement(bankAccount, connection));
+        return bankAccount;
+    }
+
+    @Override
+    public BankAccount updateBalance(BankAccount bankAccount) {
+        jdbcTemplate.update(connection -> updateBalanceStatement(bankAccount, connection));
+        return bankAccount;
+    }
+
+    private PreparedStatement updateBalanceStatement(BankAccount bankAccount, Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "UPDATE BankAccount Set balance = ? WHERE iban = ?");
+        preparedStatement.setDouble(1, bankAccount.getBalance());
+        preparedStatement.setString(2, bankAccount.getIban());
+        return preparedStatement;
+    }
+}
