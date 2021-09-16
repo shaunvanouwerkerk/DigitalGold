43cc711bb46c6bb28b689cf7571d3657d29e4517
Fiona Gray <fiona.gray@hva.nl>
2021-09-16 16:14:37 +0200

    MySQl testclass aangemaakt voor methode findAssetPriceByAssetCode

---
+++ src/test/java/com/example/digital_gold/repository/MySqlJdbcAssetPriceDaoTest.java
@@ -0,0 +1,46 @@
+package com.example.digital_gold.repository;
+
+import com.example.digital_gold.domain.AssetPrice;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/**
+ * @author Fiona Gray
+* */
+
+// LocalDateTime en SQL query zijn niet compatible met H2 databse.
+// Deze methode moet getest worden met MySQL DB, maar is wegens ()now niet continu te testen.
+@SpringBootTest
+public class MySqlJdbcAssetPriceDaoTest {
+
+    private AssetPriceDao assetPriceDaoTest;
+
+    @Autowired
+    public MySqlJdbcAssetPriceDaoTest(AssetPriceDao assetPriceDaoTest) {
+        this.assetPriceDaoTest = assetPriceDaoTest;
+    }
+
+    @Test
+    public void assetPriceDaoNotNull() {
+        assertThat(assetPriceDaoTest).isNotNull();
+    }
+
+    @Test
+    public void findPriceByAssetCode() {
+        LocalDateTime datetime = dateTimeFormatter("2021-09-16 15:58:15");
+        AssetPrice expected = new AssetPrice(null, 3964, datetime);
+        AssetPrice actual = assetPriceDaoTest.findAssetPriceByAssetCode("ETH");
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public LocalDateTime dateTimeFormatter (String resultSet) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String sqlDateTime = resultSet;
+        LocalDateTime dateTime = LocalDateTime.parse(sqlDateTime, formatter);
+        return dateTime;
+    }
+}
